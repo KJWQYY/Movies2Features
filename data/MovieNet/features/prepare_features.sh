c992f429feba20113fd26675c5537f93caed9f80
KJWQYY <56867443+KJWQYY@users.noreply.github.com>
2025-09-24 15:41:45 +0800

    Create prepare_features.sh

---
+++ data/MovieNet/features/prepare_features.sh
@@ -0,0 +1,5 @@
+python prepare_split_testset.py
+python prepare_get_visual_features_ori.py
+python prepare_get_topic_features.py
+python ../../../stage_one.py
+python prepare_get_features_context_L.py
